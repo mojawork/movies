1d24a1d73cda1e521412aa433a65e3a3dfcf076b
lungwitz <lungwitz@moja.work>
2026-03-29 23:51:44 +0200

    first step movies

--- src/api/add_movie.php
+++ src/api/add_movie.php
@@ -2,6 +2,15 @@
 // src/api/add_movie.php
 
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 require_once __DIR__ . '/../config/database.php';
 /** @var PDO $pdo */
